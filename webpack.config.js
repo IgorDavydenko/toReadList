5377e838fb4245d453509335026d1130ac463666
IgorDavydenko <4_me@li.ru>
2021-04-20 21:44:34 +0300

    start task

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/js/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[hash].js"
+    },
+    devServer: {
+        port: 3001
+    },
+    plugins: [
+        new HTMLWebpackPlugin({ template: "./src/index.html"}),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  "style-loader",
+                  "css-loader",
+                  "sass-loader",
+                ],
+            }
+        ]
+    }
+
+}
